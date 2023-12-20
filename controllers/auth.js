c3b22ea40b21c4a4123738871ea91206206f5238
chryzcode <alabaolanrewaju13@gmail.com>
2023-12-20 16:42:19 +0100

    push

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,19 @@
 const User = require('../models/User')
-const {StatusCodes} = require('http-status-codes')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
+const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-  res.json(req.body);
+  const { name, email, password } = req.body
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+  const tempUser = {name, email, password:hashedPassword}
+  if (!name || !email || !password) {
+     throw new BadRequestError('Please provide name, email and password')
+  }
+  const user = await User.create({...tempUser})
+  res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = async (req, res) => {
