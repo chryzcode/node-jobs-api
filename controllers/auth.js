dab502dd339e3696a49e58e77850a69aadfb2ae7
chryzcode <alabaolanrewaju13@gmail.com>
2023-12-20 17:10:33 +0100

    push

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,12 @@
-const User = require('../models/User')
-const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs')
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body
-
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
-  const tempUser = {name, email, password:hashedPassword}
-  if (!name || !email || !password) {
-     throw new BadRequestError('Please provide name, email and password')
-  }
-  const user = await User.create({...tempUser})
-  res.status(StatusCodes.CREATED).json(user);
+  const user = await User.create({ ...req.body });
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", { expiresIn: "30d" });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token});
 };
 
 const login = async (req, res) => {
