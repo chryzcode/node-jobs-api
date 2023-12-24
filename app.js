0a200bb471e73e8c02da137f0c19b0a0215185db
chryzcode <alabaolanrewaju13@gmail.com>
2023-12-25 00:34:41 +0100

    push

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.use(xss())
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
 
+app.get("/", (req, res) => {
+  res.send("jobs api");
+});
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
